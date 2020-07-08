8a61880bdef95668fd7455e191255eea6e557b1e
A-Tione <heyanggo@gmail.com>
2020-07-08 20:55:57 +0800

    新增登录失败用例

--- cypress/integration/examples/demo.spec.js
+++ cypress/integration/examples/demo.spec.js
@@ -1,5 +1,5 @@
 describe('cloud-auth', ()=> {
-  it('登录', ()=> {
+  it('登录成功', ()=> {
     cy.visit("http://cloud.bp.123cx.com/#/login")
     cy.get('[type="autoComplete"]').type("11100000001")
     cy.get('[type="password-plus"]').type("a123456")
@@ -10,6 +10,14 @@ describe('cloud-auth', ()=> {
     cy.wait(60)
     cy.get('.go').first().click()
     cy.url().should('include', 'system.bp.123cx.com')
+  })
+  it('登录失败', ()=> {
+    cy.visit("http://cloud.bp.123cx.com/#/login")
+    cy.get('[type="autoComplete"]').type("11100000001")
+    cy.get('[type="password-plus"]').type("a1234561")
+    cy.wait(60)
+    cy.get("button").click()
+    cy.contains('账号或密码错误').should('exist')
   })
 })
 
